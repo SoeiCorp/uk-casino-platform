2daa078331ca51e8d8d3836bd87ba0a363931955
Sarunpong Lapsuksathit <6432163521@student.chula.ac.th>
2024-11-20 17:08:47 +0700

    Test deployment on Sepolia

--- smart-contract/hardhat.config.ts
+++ smart-contract/hardhat.config.ts
@@ -1,8 +1,20 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import dotenv from 'dotenv'; 
+
+dotenv.config();
+const { API_URL, PRIVATE_KEY } = process.env;
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.27",
+    solidity: "0.8.27",
+    defaultNetwork: "sepolia",
+    networks: {
+        hardhat: {},
+        sepolia: {
+            url: API_URL as string,
+            accounts: [`0x${PRIVATE_KEY}`],
+        },
+    },
 };
 
 export default config;
