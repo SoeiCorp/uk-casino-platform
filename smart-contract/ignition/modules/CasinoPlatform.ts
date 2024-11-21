9aebd34d8f8cd6627ef795b6b588b60b7f62b71d
Sarunpong Lapsuksathit <6432163521@student.chula.ac.th>
2024-11-21 11:05:01 +0700

    Revert "Merge pull request #1 from SoeiCorp/smart-contract"
    This reverts commit 3ac016af7a5b00ce84ab41557788b1fa638758b6, reversing
    changes made to fd8a93258fe782f0778832e755b290e87f580ef7.

--- smart-contract/ignition/modules/CasinoPlatform.ts
+++
@@ -1,9 +0,0 @@
-const { buildModule } = require("@nomicfoundation/hardhat-ignition/modules");
-
-const CasinoPlatformModule = buildModule("CasinoPlatformModule", (m: any) => {
-  const casinoPlatform = m.contract("CasinoPlatform");
-
-  return { casinoPlatform };
-});
-
-module.exports = CasinoPlatformModule;
